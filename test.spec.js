7579e23ab502c0b4ceaa249277f37bf45c5f971c
John Hannagan <weetbix_@hotmail.com>
2022-09-07 14:39:52 +0800

    Add compat package test

--- test.spec.js
+++ test.spec.js
@@ -1,6 +1,6 @@
 
 
-import {ref, onMounted, defineComponent} from 'vue';
+import {ref, onMounted, defineComponent, configureCompat} from 'vue';
 import {it, expect} from 'vitest';
 import {render} from '@testing-library/vue';
 
@@ -33,6 +33,13 @@ const TestAsync = defineComponent({
         };
     },
 });
+
+// Ensure we are using the compat build
+if(process.env.COMPAT_MODE) {
+    it('should be compat mode', () => {
+        expect(configureCompat).toBeDefined();
+    })
+}
 
 it('should show onMount text', async () => {
     const {findByText} = render(TestAsync);
