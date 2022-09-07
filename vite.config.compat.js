7a71bca673832e3ca82e640c0453a90beb59a8da
John Hannagan <weetbix_@hotmail.com>
2022-09-07 13:44:52 +0800

    fix test-utils

--- vite.config.compat.js
+++ vite.config.compat.js
@@ -3,10 +3,15 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    deps: {
+      inline: [
+        '@vue/test-utils',
+      ]
+    }
   },
   resolve: {
     alias: {
       vue: '@vue/compat',
     }
-  }
+  },
 })
